ace27cf407dff444108e518e501db9d133a92158
sherly.septiani <sherly.septiani@bytedance.com>
2026-05-07 17:40:36 +0700

    ignore type checking

--- next.config.ts
+++ next.config.ts
@@ -12,9 +12,8 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true }
 };
 
 export default nextConfig;
